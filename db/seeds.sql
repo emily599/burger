c4da5a497cf08f69f632e8a2f6597d12a01d6c84
Emily <emilydoan52@gmail.com>
2019-01-25 18:59:10 -0800

    finsih

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Salmon');
+INSERT INTO burgers (burger_name) VALUES ('Cheesesteak');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken with Blue Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom and Aoli', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDonalds', true);
+INSERT INTO burgers (burger_name) VALUES ('Wendys');
